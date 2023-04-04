ddc0a4e11eacccb5a88c4e093c9df720857f421f
nik <lekares@gmail.com>
2023-04-04 21:39:28 +0700

    task47

---
+++ Program47.cs
@@ -0,0 +1,45 @@
+﻿// Задача 47: Задайте двумерный массив размером m×n, заполненный случайными 
+//вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+Console.Write("Введите m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.Clear();
+Console.WriteLine($"m = {m}, n = {n}.");
+
+double[,] array = new double[m, n];
+
+NewArray(array);
+
+SecondArray(array);
+
+Console.WriteLine();
+
+void NewArray(double[,] array)
+{
+  for (int i = 0; i < m; i++)
+  {
+    for (int j = 0; j < n; j++)
+    {
+      array[i, j] = new Random().NextDouble() * 20 - 10;
+    }
+  }
+}
+
+void SecondArray (double[,] array){
+for (int i = 0; i < m; i++)
+  {
+      for (int j = 0; j < n; j++)
+      {
+        double Number = Math.Round(array[i, j], 1);
+        Console.Write(Number + " ");
+      }
+      Console.WriteLine();
+  }
+}
